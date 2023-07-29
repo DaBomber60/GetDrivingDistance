37b3509705c96dc8d599accfc11489161866283f
DaBomber60 <46601599+DaBomber60@users.noreply.github.com>
2023-07-29 21:45:55 +0800

    Script now calculates km distance for single address

--- script.py
+++ script.py
@@ -2,10 +2,17 @@ import requests
 import json
 import pandas as pd
 from geopy.geocoders import Nominatim
+import urllib.request
+from flatten_json import flatten
 
 geolocator = Nominatim(user_agent="email")
 
 testAddress = "addressHere"
+
+homeAddressLat = lat
+homeAddressLon = lon
+
+trips = []
 
 def getCoords(address):
     location = geolocator.geocode(address)
@@ -13,10 +20,19 @@ def getCoords(address):
     lon = location.longitude
     return lat, lon
 
+def getDriveDistance(lat, lon):
+    with urllib.request.urlopen(f"https://router.project-osrm.org/route/v1/car/{homeAddressLon},{homeAddressLat};{lon},{lat}?overview=false") as url:
+        route = json.load(url)
+        flattened = flatten(route)
+        distancem = flattened['routes_0_distance']
+        distance = round(distancem/1000,2)
+        return distance
+
+
 def main():
     lat, lon = getCoords(testAddress)
-    print (lat)
-    print (lon)
+    distance = getDriveDistance(lat, lon)
+    print(str(distance) + "km to " + testAddress)
     
 
 if __name__ == "__main__":
