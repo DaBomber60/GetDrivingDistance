bafbd3e077ba2a9942b1922e99d53c35dafa26c6
DaBomber60 <46601599+DaBomber60@users.noreply.github.com>
2023-07-30 00:13:58 +0800

    Final version, exports to file
    3 seconds per address, added in sample appointments.csv file

--- script.py
+++ script.py
@@ -1,18 +1,25 @@
-import requests
 import json
 import pandas as pd
 from geopy.geocoders import Nominatim
 import urllib.request
 from flatten_json import flatten
+import time
+import csv
 
-geolocator = Nominatim(user_agent="email")
+geolocator = Nominatim(user_agent="your_user_agent_here") #Nominatim likes you to use your email in case of overuse of the API
 
-testAddress = "addressHere"
+addressFile = "appointments.csv"
 
-homeAddressLat = lat
-homeAddressLon = lon
+startAddress = "1 Fleet Street, London, UK" #Your address here
 
-trips = []
+tripsFields = ['Address', 'Distance', 'Client Name']
+tripsRows = []
+
+def loadAddresses():
+    addressdf = pd.read_csv(addressFile)
+    return addressdf
+
+    #print(addressdf.to_string())
 
 def getCoords(address):
     location = geolocator.geocode(address)
@@ -21,7 +28,8 @@ def getCoords(address):
     return lat, lon
 
 def getDriveDistance(lat, lon):
-    with urllib.request.urlopen(f"https://router.project-osrm.org/route/v1/car/{homeAddressLon},{homeAddressLat};{lon},{lat}?overview=false") as url:
+    startAddressLat, startAddressLon = getCoords(startAddress)
+    with urllib.request.urlopen(f"https://router.project-osrm.org/route/v1/car/{startAddressLon},{startAddressLat};{lon},{lat}?overview=false") as url:
         route = json.load(url)
         flattened = flatten(route)
         distancem = flattened['routes_0_distance']
@@ -30,12 +38,27 @@ def getDriveDistance(lat, lon):
 
 
 def main():
-    lat, lon = getCoords(testAddress)
-    distance = getDriveDistance(lat, lon)
-    print(str(distance) + "km to " + testAddress)
-    
+    totalDistance = 0
+    addressdf = loadAddresses()
+    for i in addressdf.index:
+        #print("NUMBER " + str(i) + ": " + addressdf.at[addressdf.index[i],'address'])
+        lat, lon = getCoords(addressdf.at[addressdf.index[i],'address'])
+        distance = getDriveDistance(lat, lon)
+        totalDistance += distance
+        tripsRows.append([str(addressdf.at[addressdf.index[i],'address']), str(distance), str(addressdf.at[addressdf.index[i],'client_name'])])
+        print("Adding trip " + str(i+1), end="")
+        time.sleep(1)
+        print(".", end="")
+        time.sleep(1)
+        print(".", end="")
+        time.sleep(1)
+        print(". Done!")
+    totalDistance = round(totalDistance,2)
+    print("The total distance to all appointments is " + str(totalDistance) + "km, for a total rount trip distance of " + str(totalDistance*2) + "km!")
+    with open('trips.csv', 'w') as f:
+        write = csv.writer(f)
+        write.writerow(tripsFields)
+        write.writerows(tripsRows)
 
 if __name__ == "__main__":
     main()
-
-    
